5896952e5937633c7bc3fe00dcc892f6c83c5871
aaleexcaazaallaa <acazper592@g.educaand.es>
2023-11-09 14:42:16 +0100

    Imc Alejandro Javier

---
+++ IndiceMasaAlejandroJavier/app/src/main/java/es/iesjandula/indicemasaalejandrojavier/InitialScreen.kt
@@ -0,0 +1,11 @@
+package es.iesjandula.indicemasaalejandrojavier
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InitialScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_initial_screen)
+    }
+}
